d9892eb94cb8a2fc369d72b5377f2d1dac168263
Lukeveille <luke@lukeleveille.com>
2018-10-20 16:02:52 -0400

    set up an object

--- application.js
+++ application.js
@@ -1,4 +1,10 @@
-var hello = <h1>Hello World</h1>
+var item = {
+  name: "gravy",
+  price: 5
+}
+
+var str = "World!"
+var hello = <h1>Hello {str} Time to get that ${item.price} {item.name}.</h1>
 
 ReactDOM.render(
   hello,
