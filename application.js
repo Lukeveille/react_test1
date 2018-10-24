d17a4b92a7358bab93a703bbf990e5dad34425f3
Lukeveille <luke@lukeleveille.com>
2018-10-23 21:08:26 -0400

    Completed first tutorial

--- application.js
+++ application.js
@@ -1,35 +1,43 @@
-// Objects
-var item = {
-  name: "gravy",
-  stock: 2,
-  price: 5,
-  imageURL: "https://i.pinimg.com/originals/a8/f9/d3/a8f9d33962b13ba82f029a34252e14a4.jpg"
+// Set up header and list items
+function ShoppingTitle(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>Total: {props.numItems}</h2>
+    </div>
+  )
 }
-var styleObject = {
-  backgroundColor: '#666',
-  color: '#FFF',
-  fontSize: 25,
-}
-
-// Logic
-if (item.stock < 1) {
-  var element = <h1>The item named {item.name} is not in stock</h1>
-} else {
-  var element = <h1 style={styleObject}>The item named {item.name} and has {item.stock} units in stock at ${item.price} costing a total of ${multiply(item.stock, item.price)}</h1> 
+function ListItem(props) {
+  return <li>{props.item}</li>
 }
 
-var allofit = <div><Hello name="Luke"/>{element}</div>
+// Build shopping list
+function ShoppingList(props) {
+  return (
+    <div>
+      <h3>{props.header}</h3>
+      <ol>
+        <ListItem item = {props.items[0]}/>
+        <ListItem item = {props.items[1]}/>
+        <ListItem item = {props.items[2]}/>
+      </ol>
+    </div>
+  )
+}
 
-// React DOM
+// Assemble page
+function ShoppingApp(props) {
+  return (
+    <div>
+      <ShoppingTitle title = "My Shopping List" numItems = "9"/>
+      <ShoppingList header = "Food" items = {[ "Apple","Bread","Cheese"]}/>
+      <ShoppingList header = "Clothes" items = {[ "Shirt","Pants","Hat"]}/>
+      <ShoppingList header = "Supplies" items = {[ "Pen","Paper","Glue"]}/>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  allofit,
+  <ShoppingApp/>,
   document.getElementById("root")
 )
-
-// Functions
-function multiply(a, b) {
-  return a * b
-}
-function Hello(arg) {
-  return <h2>Hello {arg.name}!</h2>
-}
